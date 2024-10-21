fd78547cae64f9737d49dc244329a4716e9bc35e
fraguada <fraguada@gmail.com>
2024-10-21 17:54:34 +0200

    Updated LayoutGen to use zones

--- src/commands/layoutGen.cs
+++ src/commands/layoutGen.cs
@@ -7,62 +7,117 @@
 //       e.g. #r "System.Text.Json.dll"
 //       e.g. #r "path/to/your/Library.dll"
 //       e.g. #r "path/to/your/Plugin.gha"
+// #! csharp
 
 using System;
+
 using Rhino;
 using Rhino.Geometry;
 using Rhino.DocObjects;
 
-var numCells = 5;
 var doc = RhinoDoc.ActiveDoc;
 
+// some layout parameters
 var width = 297;
 var height = 210;
 var margin = 5;
 var dWidth = 100;
 var dHeight = 100;
 
-for (int i = 0; i < numCells; i ++)
+var zone_param_text_height = 6;
+var zone_param_fontName = "Arial";
+
+var displayMode = Rhino.Display.DisplayModeDescription.FindByName("Rendered_WS");
+if( null == displayMode)
+    displayMode = Rhino.Display.DisplayModeDescription.FindByName("Rendered");
+
+//keep the view you had before starting the script
+var og_activeView = doc.Views.ActiveView;
+var og_layer_index = doc.Layers.CurrentLayerIndex;
+
+//check if we have the detail layers available?
+Layer layouts_layer = doc.Layers.FindName("Layouts");
+Layer details_layer = doc.Layers.FindName("Details");
+Layer txt_layer = doc.Layers.FindName("Text");
+
+
+// We will check if the parent layer exists
+// We could also check if the other layers exists, but we will leave that as an exercise to the participants
+
+if(null == layouts_layer) 
 {
-    var objs = doc.Objects.FindByUserString("cell", i.ToString(), false);
-    //var filter = new  ObjectEnumeratorSettings();
-    //filter.NameFilter = "cell" + i.ToString();
-    //var cell = doc.Objects.FindByFilter();
     
-    var numObjs = objs.Length;
-    Console.WriteLine("Number of objects: {0}", objs.Length);
+    int layouts_layer_index = doc.Layers.Add("Layouts", System.Drawing.Color.Black);
+    layouts_layer = doc.Layers.FindIndex(layouts_layer_index);
 
-    var oldAV = doc.Views.ActiveView;
+    int details_layer_index = doc.Layers.Add("Details", System.Drawing.Color.Black);
+    details_layer = doc.Layers.FindIndex(details_layer_index);
+    details_layer.ParentLayerId = layouts_layer.Id;
 
-    for ( int j = 0; j < numObjs; j ++) 
-    {
-        var obj = objs[j];
-        var bb = objs[j].Geometry.GetBoundingBox(false);
-        //doc.Objects.AddBox( new Box(bb));
-
-        var cell = obj.Attributes.GetUserString("cell");
-        var area = obj.Attributes.GetUserString("area");
-        var level = obj.Attributes.GetUserString("level");
-
-        var pv = doc.Views.AddPageView("Entity: " + i.ToString() + "-" + j.ToString(), width, height);
-
-        var planDetail = pv.AddDetailView("Plan", new Point2d(margin,height-dHeight), new Point2d(margin+dWidth,height-margin), Rhino.Display.DefinedViewportProjection.Top);
-
-        var vp = planDetail.Viewport;
-        var res = vp.ZoomBoundingBox(bb);
-        planDetail.CommitViewportChanges();
-        //planDetail.CommitChanges();
-
-        // text
-        doc.Views.ActiveView = pv;
-        doc.Objects.AddTextDot("Entity: " + i.ToString() + "-" + j.ToString(),new Point3d(100,100,0));
-
-        doc.Views.ActiveView = oldAV;
-    }
-
-    
+    int txt_layer_index = doc.Layers.Add("Text", System.Drawing.Color.Black);
+    txt_layer = doc.Layers.FindIndex(txt_layer_index);
+    txt_layer.ParentLayerId = layouts_layer.Id;
 
 }
 
+// go through zones
+var zones = doc.Objects.FindByLayer("ZONES");
+Console.WriteLine("Nº of Zones: {0}",zones.Length);
+
+for (int i = 0; i < zones.Length; i ++) 
+{
+    int zone_index = i + 1; // zone object name is 1-4
+    var zone_name = zone_index.ToString();
+    var zone = Array.Find(zones, zone => zone.Name == zone_name); //find the zone with the current name
+    var zone_bb = zone.Geometry.GetBoundingBox(false); // get the zone BB
+    
+    var pv = doc.Views.AddPageView("Zone: " + zone_name, width, height); //add page view aka layout
+
+    var detail = pv.AddDetailView("View", new Point2d(margin,height-dHeight), new Point2d(margin+dWidth,height-margin), Rhino.Display.DefinedViewportProjection.Perspective); // add a detail to the layout
+    detail.Attributes.LayerIndex = details_layer.Index; // change to detail layer
+
+    // edit detail vp
+    var vp = detail.Viewport;
+    vp.ChangeToParallelProjection(true);
+    vp.DisplayMode = displayMode;
+    vp.ZoomBoundingBox(zone_bb);
+    detail.CommitViewportChanges();
+
+    doc.Views.ActiveView = pv; //switch the active view to the layout so new objects land here
+
+    // text
+    var txt_oa = new Rhino.DocObjects.ObjectAttributes();
+    txt_oa.LayerIndex = txt_layer.Index; // change to the text layer
+    doc.Objects.AddTextDot("Zone: " + zone_name, new Point3d(100,100,0), txt_oa );
+
+    // get zone data
+    var builtGFA = zone.Attributes.GetUserString("BuiltGFA");
+    var landuseGFA = zone.Attributes.GetUserString("LanduseGFA");
+    var totalGFA = zone.Attributes.GetUserString("TotalGFA");
+    var landuses = zone.Attributes.GetUserString("Landuses");
+    var ratio = zone.Attributes.GetUserString("Ratio");
+
+    var builtGFA_plane = Plane.WorldXY;
+    builtGFA_plane.Origin = new Point3d(150, height - 20, 0);
+    var builtGFA_text = "Built GFA: " + builtGFA + " m2";
+    doc.Objects.AddText(builtGFA_text, builtGFA_plane, zone_param_text_height, zone_param_fontName, false, false, txt_oa);
+
+    var landuseGFA_plane = Plane.WorldXY;
+    landuseGFA_plane.Origin = new Point3d(150, height - 30, 0);
+    var landuseGFA_text = "Landuse GFA: " + landuseGFA + " m2";
+    doc.Objects.AddText(landuseGFA_text, landuseGFA_plane, zone_param_text_height, zone_param_fontName, false, false, txt_oa);
+
+    var totalGFA_plane = Plane.WorldXY;
+    totalGFA_plane.Origin = new Point3d(150, height - 40, 0);
+    var totalGFA_text = "Total GFA: " + totalGFA + " m2";
+    doc.Objects.AddText(totalGFA_text, totalGFA_plane, zone_param_text_height, zone_param_fontName, false, false, txt_oa);
+
+    var ratio_plane = Plane.WorldXY;
+    ratio_plane.Origin = new Point3d(150, height - 50, 0);
+    var ratio_text = "Ratio: " + ratio + " %";
+    doc.Objects.AddText(ratio_text, ratio_plane, zone_param_text_height, zone_param_fontName, false, false, txt_oa);
+
+    doc.Views.ActiveView = og_activeView; //reset view
 
 
+}
