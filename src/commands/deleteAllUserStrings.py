bbe72e1da911b141513004f3852d8d86171bc594
fraguada <fraguada@gmail.com>
2024-10-23 08:44:49 +0200

    update LayoutGen to make layouts for plots

--- src/commands/deleteAllUserStrings.py
+++ src/commands/deleteAllUserStrings.py
@@ -4,7 +4,6 @@ import rhinoscriptsyntax as rs
 
 objs = rs.AllObjects()
 for obj in objs:
-    print(obj)
     keys = rs.GetUserText(obj, None, False)
     for key in keys:
         value = rs.GetUserText(obj, key, False)
--- src/commands/layoutGen.cs
+++ src/commands/layoutGen.cs
@@ -61,21 +61,26 @@ if(null == layouts_layer)
 
 }
 
-// go through zones
+// collect objects
 var zones = doc.Objects.FindByLayer("ZONES");
 Console.WriteLine("Nº of Zones: {0}",zones.Length);
-var bldgs = doc.Objects.FindByUserString("Type", "Building", false);
+var plots = doc.Objects.FindByLayer("PLOTS");
+Console.WriteLine("Nº of Plots: {0}",plots.Length);
+var bldgs = doc.Objects.FindByLayer("BUILDING PADS");
 Console.WriteLine("Nº of Bldgs: {0}",bldgs.Length);
 
 for (int i = 0; i < zones.Length; i ++) 
 {
     int zone_index = i + 1; // zone object name is 1-4
     var zone_name = zone_index.ToString();
+    Console.WriteLine("--- Zone {0} ---",zone_name);
     var zone = Array.Find(zones, zone => zone.Name == zone_name); //find the zone with the current name
     var zone_bb = zone.Geometry.GetBoundingBox(false); // get the zone BB
     zone_bb.Inflate(10);
         
     var pv = doc.Views.AddPageView("Zone: " + zone_name, width, height); //add page view aka layout
+
+    doc.Views.ActiveView = pv; //switch the active view to the layout so new objects land here
 
     var detail = pv.AddDetailView("View", new Point2d(margin,height-dHeight), new Point2d(width - margin,height-margin), Rhino.Display.DefinedViewportProjection.Perspective); // add a detail to the layout
     detail.Attributes.LayerIndex = details_layer.Index; // change to detail layer
@@ -87,13 +92,13 @@ for (int i = 0; i < zones.Length; i ++)
     vp.ZoomBoundingBox(zone_bb);
     detail.CommitViewportChanges();
 
-    doc.Views.ActiveView = pv; //switch the active view to the layout so new objects land here
+    
 
     // text
     var txt_oa = new Rhino.DocObjects.ObjectAttributes();
     txt_oa.LayerIndex = txt_layer.Index; // change to the text layer
 
-    // get zone data
+    // get zone metrics
     var builtGFA = zone.Attributes.GetUserString("BuiltGFA");
     var landuseGFA = zone.Attributes.GetUserString("LanduseGFA");
     var landuseGFA_uses = landuseGFA.Split(',', StringSplitOptions.None);
@@ -127,9 +132,48 @@ for (int i = 0; i < zones.Length; i ++)
     zone_param_plane.Origin = new Point3d(margin, (height - dHeight) - zone_param_text_height * 2 - 10, 0);
     doc.Objects.AddText(zone_param_text, zone_param_plane, zone_param_text_height, zone_param_fontName, false, false, txt_oa);
 
+    // PLOTS
+
+    var zone_plots = Array.FindAll(plots, plot => plot.Name.StartsWith(zone_name));
+    Console.WriteLine("Nº Plots in Zone {0}: {1}",zone_name, zone_plots.Length);
+
+    //go through plots
+
+    foreach (var plot in zone_plots)
+    {
+
+        var plot_bb = plot.Geometry.GetBoundingBox(false); // get the zone BB
+        plot_bb.Inflate(10);
+
+        //get plot metrics
+        var p_builtGFA = plot.Attributes.GetUserString("BuiltGFA");
+        var p_landuseGFA = plot.Attributes.GetUserString("LanduseGFA");
+        var p_landuseGFA_uses = p_landuseGFA.Split(',', StringSplitOptions.None);
+        var p_totalGFA = plot.Attributes.GetUserString("TotalGFA");
+        var p_landuses = plot.Attributes.GetUserString("Landuses");
+        var p_ratio = plot.Attributes.GetUserString("Ratio");
+
+        var p_pv = doc.Views.AddPageView("Plot: " + plot.Name, width, height); //add page view aka layout
+
+        doc.Views.ActiveView = p_pv; //switch the active view to the layout so new objects land here
+
+        var p_detail = p_pv.AddDetailView("View", new Point2d(margin,height-dHeight), new Point2d(width - margin,height-margin), Rhino.Display.DefinedViewportProjection.Perspective); // add a detail to the layout
+        p_detail.Attributes.LayerIndex = details_layer.Index; // change to detail layer
+
+        // edit detail vp
+        var p_vp = p_detail.Viewport;
+        p_vp.ChangeToParallelProjection(true);
+        p_vp.DisplayMode = displayMode;
+        p_vp.ZoomBoundingBox(plot_bb);
+        p_detail.CommitViewportChanges();
+
+        var plot_plane = Plane.WorldXY;
+        plot_plane.Origin = new Point3d(margin, (height - dHeight) - zone_param_text_height * 2 , 0);
+        var plot_text = "Plot " + plot.Name;
+        doc.Objects.AddText(plot_text, plot_plane, zone_param_text_height * 2, zone_param_fontName, false, false, txt_oa);
+
+    }
+
     doc.Views.ActiveView = og_activeView; //reset view
-
-    
-
 
 }
