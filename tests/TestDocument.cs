5bbe6f70b2bfef2bb69b631954f3aadfddd4c2a0
fraguada <fraguada@gmail.com>
2024-11-05 16:43:34 +0100

    Adding basic test

---
+++ tests/TestDocument.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+using Rhino;
+using Rhino.Testing.Fixtures;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace WorkflowToolsNUnitTests
+{
+    [TestFixture]
+    public sealed class TestWorkflowTools : RhinoTestFixture
+    {
+        [Test]
+        public void TestSampleModelExists()
+        {
+            string output = Path.GetDirectoryName(GetType().Assembly.Location);
+            string zipPath = Path.Combine(output, @"..\ref\24.11.05_MasterplanBuildings_Start.3dm.zip");
+            string modelPath = Path.Combine(output, @"..\ref\24.11.05_MasterplanBuildings_Start.3dm");
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, modelPath);
+
+            Assert.FileExists(modelPath);
+            Assert.DoesNotThrow(() => RhinoDoc.Open(modelPath, out bool _));
+        }
+
+    }
+}
