843b8e8363d4ea0090cdeef4aa6c439a31ece571
fraguada <fraguada@gmail.com>
2024-11-06 17:48:18 +0100

    Install plugin and use a command in a test

--- tests/TestDocument.cs
+++ tests/TestDocument.cs
@@ -24,13 +24,24 @@ namespace WorkflowToolsNUnitTests
             {
                 Directory.Delete(filesPath, true);
             }
-            
+
             System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, filesPath);
 
             string modelPath = Path.Combine(filesPath, @"24.11.05_MasterplanBuildings_Start.3dm");
 
             Assert.IsTrue(File.Exists(modelPath));
             Assert.DoesNotThrow(() => RhinoDoc.Open(modelPath, out bool _));
+
+            var doc = RhinoDoc.ActiveDoc;
+            var hatches = doc.Objects.FindByObjectType(Rhino.DocObjects.ObjectType.Hatch);
+            var hatch = hatches[0];
+            var userStringCount = hatch.Attributes.GetUserStrings().Count;
+
+            Assert.AreEqual(0, userStringCount);
+            Assert.DoesNotThrow(() => RhinoApp.RunScript("WT_CalculateMetrics", false));
+            //Assert.DoesNotThrow(() => RhinoApp.InvokeOnUiThread(new Action(() => RhinoApp.RunScript("WT_CalculateMetrics", false))));
+            Assert.IsTrue(hatch.Attributes.GetUserStrings().Count > 0);
+
         }
     }
 }
